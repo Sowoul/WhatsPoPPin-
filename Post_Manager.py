e248338d6f587f4dcfbaf09af263e327ffff7b07
Ojaswa Sharma <93905595+Sowoul@users.noreply.github.com>
2024-01-29 23:03:15 +0530

    Update Post_Manager.py

--- Post_Manager.py
+++ Post_Manager.py
@@ -1,8 +1,9 @@
 from collections import deque
 import json
 """
-For O(1) Time Complexity when Adding a new post, using a deque.
-"""
+Simple PostManager Object that used a queue to store the image, to keep O(1) time complexity, 
+while returning in time based order.
+""" 
 class PostManager:
     def __init__(self, filename="posts.json"):
         self.filename = filename
@@ -15,7 +16,9 @@ class PostManager:
                 self.posts.extend(json.load(file))
         except FileNotFoundError:
             pass 
-    
+    def delete_posts(self):
+        self.posts.popleft()
+        self.save_posts()
     def save_posts(self):
         with open(self.filename, 'w') as file:
             json.dump(list(self.posts), file)
@@ -26,3 +29,6 @@ class PostManager:
 
     def get_all_posts(self):
         return self.posts
+if __name__ == '__main__':
+    postmanager = PostManager()
+    postmanager.delete_posts()
